9f95a1f201160df0e5a784289bb86e75f99c6c7c
Suzy Kersten <suzykersten72@outlook.com>
2018-02-25 22:15:06 -0600

    added back button to About activity

--- app/src/main/java/com/harrisonwelch/simon/AboutActivity.java
+++ app/src/main/java/com/harrisonwelch/simon/AboutActivity.java
@@ -2,6 +2,7 @@ package com.harrisonwelch.simon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends Activity {
 
@@ -9,5 +10,12 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
