83e74939ddd8e415928a494777a20c1b8414a129
Suzy Kersten <suzykersten72@outlook.com>
2018-03-11 22:20:15 -0500

    made about look nice

--- app/src/main/java/com/harrisonwelch/simon/AboutActivity.java
+++ app/src/main/java/com/harrisonwelch/simon/AboutActivity.java
@@ -2,7 +2,9 @@ package com.harrisonwelch.simon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
@@ -10,6 +12,9 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView tv = findViewById(R.id.textView_soundSource);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
 
         findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
             @Override
